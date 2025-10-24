0d86c01ea9a8f79c3ae4240efe62fb8ed9870052
davosab <davidsabau72@gmail.com>
2025-10-24 10:22:39 +0300

    catchup

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,29 @@
-<script setup></script>
+<script setup>
+
+  const name = "Mariushkuta";
+
+  import {onMounted, onUpdated} from "vue";
+  onMounted(() => {
+    console.log("onMounted()");
+  });
+
+  onUpdated(() => {
+    console.log("onUpdated()");
+  });
+
+</script>
 
 <template>
-  <h1>You did it!</h1>
+  <h1>You did it {{name}}!</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  
+  <button @click="console.log('Why didn\'t you listen?')">Dont click me</button>
+  <br><br>
+  
+  <input type="text" @change="console.log('input changed')"/>
 </template>
 
 <style scoped></style>
