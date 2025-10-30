3ac91cd07fcf1dc26f74c372eeac7ed5b809847c
davosab <davidsabau72@gmail.com>
2025-10-30 10:34:11 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script setup>
 
   const name = "Mariushkuta";
+  const link = ref("https:/emanuel.ro");
+  const number = ref(null);
+  const counter = ref(0);
 
-  import {onMounted, onUpdated} from "vue";
+  import {ref, onMounted, onUpdated} from "vue";
   onMounted(() => {
     console.log("onMounted()");
   });
@@ -10,6 +13,18 @@
   onUpdated(() => {
     console.log("onUpdated()");
   });
+
+  const clickHandler = () => {
+    link.value = "https:/youtube.com";
+  }
+
+  const randomNumber = () => {
+    number.value = Math.random().toFixed(2);
+  }
+
+  const increment = () => {
+    counter.value++;
+  }
 
 </script>
 
@@ -20,10 +35,28 @@
     documentation
   </p>
   
-  <button @click="console.log('Why didn\'t you listen?')">Dont click me</button>
+  <button @click="clickHandler">Click me</button>
   <br><br>
-  
+
+  <a :href="link" target="_blank" rel="noopener noreferrer">Visit Emanuel website</a>
+  <br>
   <input type="text" @change="console.log('input changed')"/>
+
+  <br>
+  <hr>
+  <button @click="randomNumber">Generate random number</button>
+  
+  <div v-if="number">
+    <h4>Random number is:</h4>
+
+    <div v-if="number > 0.5">Number {{ number }} is bigger than 0.5</div>
+    <div v-else>Number {{ number }} is smaller than 0.5</div>
+  </div>
+  <br>
+  <hr>
+
+  <button @click="increment">Click to increment: {{ counter }}</button>
+
 </template>
 
 <style scoped></style>
